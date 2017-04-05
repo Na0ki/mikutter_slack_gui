4b4ea4fc2ec1d8c7f0151e2f0a39712a65053ec0
ahiru <wellingtonnmaeda0524@gmail.com>
2017-04-05 16:57:51 +0900

    added rubocop settings

--- slack_gui.rb
+++ slack_gui.rb
@@ -1,21 +1,16 @@
 # -*- coding: utf-8 -*-
 
 Plugin.create(:slack_gui) do
-
-
   # 実績設定
   # @see http://mikutter.blogspot.jp/2013/03/blog-post.html
   defachievement(:slack_achieve,
                  description: '設定画面からSlackの認証をしよう',
-                 hint: 'mikutterの設定を開いてslackの認証ボタンを押そう！'
-  ) do |achievement|
+                 hint: 'mikutterの設定を開いてslackの認証ボタンを押そう！') do |achievement|
     on_slack_connected { |_| achievement.take! }
   end
 
-
   # Activity の設定
   defactivity 'slack_connection', 'Slack接続情報'
-
 
   intent :slack_channel, label: 'Slack Channelを開く' do |intent_token|
     channel = intent_token.model
@@ -36,33 +31,28 @@ Plugin.create(:slack_gui) do
     }.trap { |e| error e }
   end
 
-
   # 接続時
   on_slack_connected do |auth|
     activity :slack_connection, "Slackチーム #{auth['team']} の認証に成功しました！"
   end
-
 
   # 接続失敗時
   on_slack_connection_failed do |error|
     activity :slack_connection, "Slackチームの認証に失敗しました！: #{error} "
   end
 
-
   def image(display_url)
     connection = HTTPClient.new
     connection.get_content(display_url, 'Authorization' => "Bearer #{UserConfig['slack_token']}")
   end
 
-
   # コマンド登録
   # コマンドのslugはpost_to_slack_#{チーム名}_#{チャンネル名}の予定
   command(:post_to_slack,
           name: 'Slackに投稿する',
-          condition: lambda { |_| true },
+          condition: ->(_) { true },
           visible: true,
-          role: :postbox
-  ) do |opt|
+          role: :postbox) do |opt|
     # TODO: mikutter_slack からチャンネルリストの取得を実装する
     msg = Plugin.create(:gtk).widgetof(opt.widget).widget_post.buffer.text # postboxからメッセージを取得
     next if msg.empty?
@@ -98,5 +88,4 @@ Plugin.create(:slack_gui) do
       dialog.destroy
     end
   end
-
 end
